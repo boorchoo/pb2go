ace14e7c4853dafce074ee19864963ba31a2d59d
Milan Živadinović <boorchoo@gmail.com>
2015-02-27 16:53:32 +0100

    Added Protocol Buffers type to PHP type mapping function

--- classes/BaseGenerator.php
+++ classes/BaseGenerator.php
@@ -2,11 +2,33 @@
 
 abstract class BaseGenerator {
 	
+	protected static $phpTypeMapping = array(
+		'double' => 'float',
+		'float' => 'float',
+		'int32' => 'integer',
+		'int64' => 'integer',
+		'uint32' => 'integer',
+		'uint64' => 'integer',
+		'sint32' => 'integer',
+		'sint64' => 'integer',
+		'fixed32' => 'integer',
+		'fixed64' => 'integer',
+		'sfixed32' => 'integer',
+		'sfixed64' => 'integer',
+		'bool' => 'boolean',
+		'string' => 'string',
+		'bytes' => 'string',
+	);
+	
 	public function __construct() {
 	}
 	
 	public function toCamelCase($string) {
 		return ucfirst(preg_replace_callback('/_([a-zA-Z])/', create_function('$c', 'return strtoupper($c[1]);'), $string));
+	}
+	
+	public function getPHPType($protocolBuffersType) {
+		return isset(self::$phpTypeMapping[$protocolBuffersType]) ? self::$phpTypeMapping[$protocolBuffersType] : NULL;
 	}
 
 }
