08c3f0d39c1ca5511fd42494fc8c6b611748d1da
Milan Živadinović <boorchoo@gmail.com>
2015-03-20 16:08:33 +0100

    Refactored service client batch support

--- classes/PHPServiceClientGenerator.php
+++ classes/PHPServiceClientGenerator.php
@@ -16,13 +16,27 @@ class PHPServiceClientGenerator extends PHPGenerator {
 			echo "{$filepath}\n";
 		}
 		
+		$source = $this->generateServiceClientBatchClassSource();
+		$filepath = "{$path}/classes/JSONRPC/ServiceClient_Batch.php";
+		$res = $this->output($filepath, $source);
+		if ($res) {
+			echo "{$filepath}\n";
+		}
+		
 		foreach ($this->proto['services'] as $service) {
 			$source = $this->generateClassSource($service);
 			$filepath = "{$path}/classes/" . (empty($service['package']) ? '' : (str_replace('\\', '/', $this->getNamespace($service['package'])) . '/')) . "{$service['service']}Client.php";
 			$res = $this->output($filepath, $source);
 			if ($res) {
 				echo "{$filepath}\n";
-			} 
+			}
+			
+			$source = $this->generateBatchClassSource($service);
+			$filepath = "{$path}/classes/" . (empty($service['package']) ? '' : (str_replace('\\', '/', $this->getNamespace($service['package'])) . '/')) . "{$service['service']}Client_Batch.php";
+			$res = $this->output($filepath, $source);
+			if ($res) {
+				echo "{$filepath}\n";
+			}
 		}
 	}
 	
@@ -38,13 +52,11 @@ abstract class ServiceClient {
 
 	protected $url;
 	protected $requestHeaders;
-	protected $requests;
 	protected $id;
 
 	protected function __construct($url) {
 		$this->url = $url;
 		$this->requestHeaders = array();
-		$this->requests = array();
 		$this->id = 0;
 	}
 
@@ -74,30 +86,7 @@ abstract class ServiceClient {
 		}
 	}
 
-	protected function addRequest($method, $params, $responseClassName) {
-		if (!empty($params) && !$params->isInitialized()) {
-			throw new UninitializedMessageException();
-		}
-		$request = new Request();
-		$request->setMethod($method);
-		$request->setParams(empty($params) ? NULL : $params->toStdClass());
-		$request->setId($this->getId());
-		$this->requests[$this->getLastId()] = array(
-			'request' => $request,
-			'responseClassName' => $responseClassName,
-		);
-		return $this->getLastId();
-	}
-
-	protected function getRequestResponseClassName($id) {
-		return isset($this->requests[$id]['responseClassName']) ? $this->requests[$id]['responseClassName'] : NULL; 
-	}
-
-	public function clearRequests() {
-		$this->requests = array();
-	}
-
-	protected function getId() {
+	public function getId() {
 		return ++$this->id;
 	}
 
@@ -105,62 +94,120 @@ abstract class ServiceClient {
 		return $this->id;
 	}
 
-	protected function invoke($method = NULL, $params = NULL, $responseClassName = NULL) {
-		if (empty($method)) {
-			$requests = array();
-			foreach ($this->requests as $request) {
-				array_push($requests, $request['request']->toStdClass());
-			}
-			$data = json_encode($requests);
-		} else {
-			if (!empty($params) && !$params->isInitialized()) {
-				throw new UninitializedMessageException();
-			}
-			$request = new Request();
-			$request->setMethod($method);
-			$request->setParams(empty($params) ? NULL : $params->toStdClass());
-			$request->setId($this->getId());
-			$data = json_encode($request->toStdClass());
-		}
-
+	public function send($request) {
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $this->url);
 		curl_setopt($ch, CURLOPT_POST, 1);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($request));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 		$requestHeaders = array();
 		foreach ($this->requestHeaders as $header => $value) {
 			array_push($requestHeaders, "{$header}: {$value}");
 		}
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $requestHeaders);
-		$output = json_decode(curl_exec($ch));
-		curl_close ($ch);
+		$response = json_decode(curl_exec($ch));
+		curl_close($ch);
+		return $response;
+	}
 
-		if (is_array($output)) {
-			$results = array();
-			foreach ($output as $object) {
-				$response = Response::fromStdClass($object);
-				if ($response->hasError()) {
-					$error = $response->getError();
-					continue;
-				}
-				$responseClassName = $this->getRequestResponseClassName($response->getId());
-				$results[$response->getId()] = empty($responseClassName) ? $response->getResult() : $responseClassName::fromStdClass($response->getResult());
-			}
-			$this->clearRequests();
-			return $results;
+	protected function invoke($method, $params) {
+		if (empty($params)) {
+			throw new InvalidParams();
 		}
-
-		$response = Response::fromStdClass($output);
+		if (!$params->isInitialized()) {
+			throw new UninitializedMessageException();
+		}
+		$request = new Request();
+		$request->setMethod($method);
+		$request->setParams($params->toStdClass());
+		$request->setId($this->getId());
+		$response = Response::fromStdClass($this->send($request->toStdClass()));
 		if ($response->hasError()) {
 			$error = $response->getError();
 			throw new \Exception($error->getMessage(), $error->getCode(), NULL);
 		}
-		return empty($responseClassName) ? $response->getResult() : $responseClassName::fromStdClass($response->getResult());
+		return $response->getResult();
 	}
 
-	public function invokeAll() {
-		return $this->invoke();
+}
+
+SOURCE;
+		return $source;
+	}
+	
+	public function generateServiceClientBatchClassSource() {
+		$source = <<<'SOURCE'
+<?php
+
+/*** DO NOT MANUALLY EDIT THIS FILE ***/
+
+namespace JSONRPC;
+
+class ServiceClient_Batch {
+
+	protected $serviceClient;
+	protected $requests;
+
+	protected function __construct($serviceClient) {
+		$this->serviceClient = $serviceClient;
+		$this->requests = array();
+	}
+
+	public function getServiceClient() {
+		return $this->serviceClient;
+	}
+
+	protected function add($method, $params, $responseClassName) {
+		if (empty($params)) {
+			throw new InvalidParams();
+		}
+		if (!$params->isInitialized()) {
+			throw new UninitializedMessageException();
+		}
+		$request = new Request();
+		$request->setMethod($method);
+		$request->setParams($params->toStdClass());
+		$request->setId($this->serviceClient->getId());
+		$this->requests[$this->serviceClient->getLastId()] = array(
+			'request' => $request,
+			'responseClassName' => $responseClassName,
+			'result' => NULL,
+			'error' => NULL,
+		);
+		return $this->serviceClient->getLastId();
+	}
+
+	public function hasResult($id) {
+		return isset($this->requests[$id]['result']);
+	}
+
+	public function getResult($id) {
+		return isset($this->requests[$id]['result']) ? $this->requests[$id]['result'] : NULL;
+	}
+
+	public function hasError($id) {
+		return isset($this->requests[$id]['error']);
+	}
+
+	public function getError($id) {
+		return isset($this->requests[$id]['error']) ? $this->requests[$id]['error'] : NULL;
+	}
+
+	public function send() {
+		$requests = array();
+		foreach ($this->requests as $request) {
+			array_push($requests, $request['request']->toStdClass());
+		}
+		$objects = $this->serviceClient->send($requests);
+		foreach ($objects as $object) {
+			$response = Response::fromStdClass($object);
+			if ($response->hasError()) {
+				$this->requests[$response->getId()]['error'] = $response->getError();
+				continue;
+			}
+			$responseClassName = $this->requests[$response->getId()]['responseClassName'];
+			$this->requests[$response->getId()]['result'] = $responseClassName::fromStdClass($response->getResult());
+		}
 	}
 
 }
@@ -192,19 +239,22 @@ class {$service['service']}Client extends \JSONRPC\ServiceClient {
 		parent::__construct(\$url);
 	}
 
+	public function newBatch() {
+		return new {$service['service']}Client_Batch(\$this);
+	}
+
 SOURCE;
 		if (!empty($service['rpcs'])) {
 			foreach ($service['rpcs'] as $rpcName => $rpc) {
 				$returnsType = Registry::getType($rpc['returns']);
-				$returns = (empty($returnsType['package']) ? '' : '\\' . $this->getNamespace($returnsType['package'])) . '\\' . str_replace('.', '_', $returnsType['name']);
+				$returns = str_replace('.', '_', $returnsType['name']);
+				if ($service['package'] !== $returnsType['package']) {
+					$returns = (empty($returnsType['package']) ? '' : '\\' . $this->getNamespace($returnsType['package'])) . '\\' . $returns;
+				}
 				$source .= <<<SOURCE
 
 	public function {$rpcName}(\$params) {
-		return \$this->invoke('{$rpcName}', \$params, '{$returns}');
-	}
-
-	public function add{$rpcName}Request(\$params) {
-		return \$this->addRequest('{$rpcName}', \$params, '{$returns}');
+		return {$returns}::fromStdClass(\$this->invoke('{$rpcName}', \$params));
 	}
 
 SOURCE;
@@ -215,7 +265,51 @@ SOURCE;
 }
 
 SOURCE;
+		return $source;
+	}
+	
+	public function generateBatchClassSource($service) {
+		$namespace = $this->getNamespace($service['package']);
+		$source = <<<SOURCE
+<?php
 
+/*** DO NOT MANUALLY EDIT THIS FILE ***/
+
+
+SOURCE;
+		if (!empty($namespace)) {
+			$source .= <<<SOURCE
+namespace {$namespace};
+
+
+SOURCE;
+		}
+		$source .= <<<SOURCE
+class {$service['service']}Client_Batch extends \JSONRPC\ServiceClient_Batch {
+
+	public function __construct(\$serviceClient) {
+		parent::__construct(\$serviceClient);
+	}
+
+SOURCE;
+		if (!empty($service['rpcs'])) {
+			foreach ($service['rpcs'] as $rpcName => $rpc) {
+				$returnsType = Registry::getType($rpc['returns']);
+				$returns = (empty($returnsType['package']) ? '' : '\\' . $this->getNamespace($returnsType['package'])) . '\\' . str_replace('.', '_', $returnsType['name']);
+				$source .= <<<SOURCE
+
+	public function {$rpcName}(\$params) {
+		return \$this->add('{$rpcName}', \$params, '{$returns}');
+	}
+
+SOURCE;
+			}
+		}
+		$source .= <<<'SOURCE'
+
+}
+
+SOURCE;
 		return $source;
 	}
 
